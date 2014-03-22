7e35032b1f906ca6cadd4e63d7564be115935b0e
Bruce Alderson <bruce@napkinware.com>
2014-03-22 14:41:29 -0700

    Adds some documentation.

--- chronic-delegator.php
+++ chronic-delegator.php
@@ -12,22 +12,23 @@ if (!defined('SITE_BASE')) define('SITE_BASE', realpath(CHRONIC_BASE . '/../../'
 if (!defined('LIB_BASE')) define('LIB_BASE', realpath(CHRONIC_BASE . '/../'));
 if (!defined('PRESTO_BASE')) define('PRESTO_BASE', realpath(CHRONIC_BASE . '/../presto/'));
 
-date_default_timezone_set('America/Los_Angeles');
+// Check ChronicleMD requirements
+assert(version_compare(PHP_VERSION, '5.4.0') >= 0, 'Chronicle requires a newer version of PHP.');
 
 require PRESTO_BASE . '/lib/request.php';
 require PRESTO_BASE . '/lib/response.php';
+require CHRONIC_BASE . '/chronicle.md.php';
 
 if (isset($argc) && $argc && !array_key_exists('HTTP_HOST', $_SERVER)) {
+
+	// Handle as CLI request
+
 	require CHRONIC_BASE . '/cli.php';
 	return;
 }
 
-require CHRONIC_BASE . '/chronicle.md.php';
-
 try {
-	assert(version_compare(PHP_VERSION, '5.4.0') >= 0, 'Chronicle requires a newer version of PHP.');
-
-	// Start up the site and render the current page template
+	// Handle as HTTP request
 
 	$site = new napkinware\chronicle\site();
 	$site->go();
