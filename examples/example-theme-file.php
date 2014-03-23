1c1b0607ee09374ae476d491f5e018f2c703e1ee
Bruce Alderson <bruce@napkinware.com>
2014-03-23 13:44:17 -0700

    Updates example theme, adding debugging chunks, updating to new API.

--- examples/example-theme-file.php
+++ examples/example-theme-file.php
@@ -1,25 +1,68 @@
-<!DOCTYPE html>
-<html lang="">
-<head>
-  <meta charset="utf-8">
-	<title><?= $site->settings->name ?></title>
-	<meta name="description" content="<?= $site->settings->description ?>" />
+<?php $chronic->showPart('theme/header.php'); ?>
 
-	<link rel="stylesheet" href="/styles/main.css" />
+<style>
+body {
+	font: normal .9em/1.25em "Open Sans", "Helvetica Neue", Helvetica, sans-serif;
+}
+main, header {
+	width: 80%;
+	margin: 2em auto;
+	border-top: 4px solid rgba(0,0,0,.1);
+}
+section {
+	margin: 2em auto;
+	border-top: 1px solid rgba(0,0,0,.25);
+}
+</style>
+<main>
+	<aside>This example theme file shows the template functions and debugging functions.</aside>
 
-</head>
-<?php flush(); ?><body>
+	<section>
+		<h2>Posts</h2>
+<?php while ($post = $chronic->nextPost()) { ?>
+	<article>
+		<pre>
+File : <?php print_r($post->file); ?>
 
-<header><div>
-<h1><a href="/"><?= $site->settings->name ?> <em><?= $site->settings->tagline ?></em></a></h1>
-<div></header>
+Title: <?php print_r($post->title); ?>
 
-<main><div>
-<?= $site; ?>
-</div></main>
+URL  : <?php print_r($post->url); ?>
 
-<footer><div>
-</div></footer>
+		</pre>
+	</article>
+<?php } ?>
 
-</body>
-</html>
+
+<section class="debug">
+
+<h3>Current post</h3>
+<pre>
+<?= $chronic->debugInfo('nav'); ?>
+</pre>
+
+<h3>Post list</h3>
+<pre>
+<?= json_encode($chronic->postList(), JSON_PRETTY_PRINT); ?>
+</pre>
+
+<h3>Current file</h3>
+<pre>
+<?= $chronic->debugInfo('file'); ?>
+</pre>
+
+<h3>Template</h3>
+<pre>
+<?= $chronic->debugInfo('template'); ?>
+</pre>
+
+<h3>HTTP request</h3>
+<pre>
+<?= $chronic->debugInfo('req'); ?>
+</pre>
+</section>
+
+</main>
+
+
+
+<?php $chronic->showPart('theme/footer.php'); ?>
