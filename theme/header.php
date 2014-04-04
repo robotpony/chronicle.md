d769593e412ede2f387af9acd5ea30734ef815ca
Bruce Alderson <bruce@napkinware.com>
2014-04-04 12:56:34 -0700

    Moves styles + adds boostrap.

--- theme/header.php
+++ theme/header.php
@@ -5,7 +5,9 @@
 	<title><?= CHRONIC ?> - <?= $chronic->pageTitle(); ?></title>
 
 	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.0/less.min.js" type="text/javascript"></script>
-	<link rel="stylesheet/less" type="text/css" href="styles.less" />
+
+	<link rel="stylesheet/less" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+	<link rel="stylesheet/less" type="text/css" href="/theme/styles.less" />
 <!--[if IE]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
@@ -14,8 +16,4 @@
 
 <body id="home">
 
-	<header>
-		<h1><a href="<?= $chronic->settings->site->URL ?>"><?= $chronic->settings->site->name ?></a></h1>
-		<p><?= $chronic->settings->site->tagline ?></p>
-	</header>
 
