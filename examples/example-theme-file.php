8bc9ca4e7aa6f80ae7f05e5147dc62805d584d1c
Bruce Alderson <bruce@napkinware.com>
2014-04-04 16:32:53 -0700

    Splits out theme a bit.

--- examples/example-theme-file.php
+++ examples/example-theme-file.php
@@ -1,88 +1,26 @@
 <?php $chronic->showPart('theme/header.php'); ?>
 
-<style>
-body {
-	font: normal .9em/1.25em "Open Sans", "Helvetica Neue", Helvetica, sans-serif;
-}
-body > header h1 a {
-	color: rgba(200,125,25,.85);
-	text-decoration: none;
-}
-main > aside {
-	font-style: italic;
-}
-main, header {
-	width: 80%;
-	margin: 2em auto;
-	border-top: 4px solid rgba(0,0,0,.1);
-}
-section {
-	margin: 2em auto;
-	border-top: 1px solid rgba(0,0,0,.25);
-}
-section.debug {
-	display: none;
-}
-pre {
-	background-color: rgba(0,0,0,.05);
-	padding: .5em 1em;
-}
-
-</style>
 <main>
-	<aside>This example theme file shows the template functions and debugging functions.</aside>
+	<aside class="info">
+        <p>This example theme file shows the template functions and debugging functions.</p>
+    </aside>
 
-
-	<article>
-<?= $chronic->page()->html(); ?>
-	</article>
-
-	<h2>More posts</h2>
+	<section class="sitemap">
+		<h2>Documentation</h2>
+		<article>
+			<ul>
 <?php foreach ($chronic->posts() as $post) { ?>
-
-	<article>
-		<pre>
-File : <?php print_r($post->name); ?>
-
-Title: <?php print_r($post->title); ?>
-
-URL  : <?php print_r($post->url); ?>
-
-		</pre>
-	</article>
+				<li class="<?= $post->type; ?>"><a href="<?= $post->url; ?>" title="<?= $post->name; ?>"><?= $post->title; ?></a></li>
 <?php } ?>
+			</ul>
+		</article>
+	</section>
 
-
-<section class="debug">
-
-<h3>Entries</h3>
-<pre>
-<?= $chronic->debugInfo('entries'); ?>
-</pre>
-
-<h3>Post list</h3>
-<pre>
-<?= json_encode($chronic->postList(), JSON_PRETTY_PRINT); ?>
-</pre>
-
-<h3>Requested resource</h3>
-<pre>
-<?= $chronic->debugInfo('resource'); ?>
-</pre>
-
-<h3>HTTP request</h3>
-<pre>
-<?= $chronic->debugInfo('req'); ?>
-</pre>
-</section>
-
-<h3>Template</h3>
-<pre>
-<?= $chronic->debugInfo('template'); ?>
-</pre>
-
-</main>
-
+	<section class="posts">
+		<article>
+<?= $chronic->page()->html(); ?>
+		</article>
+	</section>
 
 
 <?php $chronic->showPart('theme/footer.php'); ?>
--- theme/header.php
+++ theme/header.php
@@ -4,14 +4,13 @@
 	<meta charset="utf-8">
 	<title><?= CHRONIC ?> - <?= $chronic->pageTitle(); ?></title>
 
-	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.0/less.min.js" type="text/javascript"></script>
-
-	<link rel="stylesheet/less" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet/less" type="text/css" href="/theme/styles.less" />
+	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.0/less.min.js" type="text/javascript"></script>
 <!--[if IE]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
-
+    
 </head>
 
 <body id="home">
