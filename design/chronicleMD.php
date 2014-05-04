e5df9a93e46ef409532a73e5f0b6453e379bb80d
Bruce Alderson <bruce@napkinware.com>
2014-05-04 16:56:54 -0700

    Initial design and prototype pass.

--- design/chronicleMD.php
+++ design/chronicleMD.php
@@ -2,31 +2,77 @@
 
 namespace robotpony\chronicleMD;
 
+/* # Chronicle 1.1 prototype
+
+	This prototype outlines the classes and calling mechanisms (to prove them out). It
+	currently generates debugging output for testing themes and evaluating the intended
+	calling sequences.
+
+
+	## Approach
+
+	These classes use magic methods to simplify the calling sequence for theme APIs. Rather
+	than force APIs to use parameters for common variable items,
+
+
+	## Missing
+*/
+
+
+/* on <event>
+
+	The event handling infrastructure for ChronicleMD
+*/
+class on {
+
+	/* Handle user-defined events */
+	public static function __callStatic($n, $a) {
+		$p = count($a) ?
+		' - ' . implode(', ', $a) : '';
+
+		return "($n$p)";
+	}
+}
+
+/* The document manager
+
+Provides acccess to folders of documents.
+
+*/
 class documents {
 
 
 	public static function __callStatic($n, $a) {
-		return array(new post(), new post());
+		return array(new document(), new document());
 	}
 }
 
-class post {
+/* A single document
+
+Provides access to the document content and metadata. This is what WordPress calls a `post`.
+
+*/
+class document {
 
 	public function __call($n, $a) {
-		return "$n\n";
+		return "$n";
 	}
 }
 
+/* The site theme
+
+*/
 class theme {
 
 	public static function __callStatic($n, $a) {
 		$p = count($a) ?
-			' - ' . implode(', ', $a) : '';
-		return "$n$p\n";
+			' (' . implode(', ', $a) . ')' : '';
+		return "$n.php$p\n";
 	}
 }
 
 
+/* Global helpers */
 
 
 function exception_handler($e) {
--- design/index.php
+++ design/index.php
@@ -1,17 +1,50 @@
 <?php use robotpony\chronicleMD as site;
 
-site\theme::startup();
+site\on::startup();
 
-site\theme::header();
 ?>
 
-<?php foreach (site\documents::blog() as $post) { ?>
+<?= site\theme::header(); ?>
 
-<?= $post->title(); ?>
-<?= $post->date(); ?>
-<?= $post->body(); ?>
+<main>
+<?php foreach (site\documents::blog(array('max-posts' => 1)) as $post) { ?>
 
+<section>
+
+	<header>
+		<h1><?= $post->title(); ?></h1>
+		<date><?= $post->date(); ?></date>
+	</header>
+
+	<article><?= $post->body(); ?></article>
+
+</section>
 
 <?php } ?>
+</main>
+
+
+<aside>
+<h2>Recent posts</h2>
+<?php
+	$params = array(
+		'max-posts' => 10,
+		'exclude-current' => true
+	);
+	foreach (site\documents::blog($params) as $doc) {
+?>
+	<a href="<?= $doc->url(); ?>"><?= $doc->title(); ?></a>
+<?php } ?>
+</aside>
+
+<aside>
+<h2>Current projects</h2>
+<?php
+	foreach (site\documents::projects_current() as $post) {
+?>
+	<a href="<?= $post->url(); ?>"><?= $post->title(); ?></a>
+<?php } ?>
+</aside>
+
 
 <?= site\theme::footer(); ?>
