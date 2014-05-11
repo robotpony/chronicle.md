81f9ea1d44fb87f324401339928f3ebee0249b2e
Bruce Alderson <balderson@discoverysoftware.com>
2014-05-10 20:55:23 -0700

    Fixes lib path.

--- design/config.php
+++ design/config.php
@@ -1,4 +1,4 @@
 <?php
 
-define('CHRONICLEMD_ROOT', realpath($_SERVER['DOCUMENT_ROOT'] . '/lib/chronicle.md/'));
+define('CHRONICLEMD_ROOT', realpath(dirname(__FILE__)));
 define('BLOG_ROOT', realpath($_SERVER['DOCUMENT_ROOT']));
