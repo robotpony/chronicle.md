39de2cfbc21229f69b49e16733ff376beb248ba2
Bruce Alderson <balderson@discoverysoftware.com>
2014-05-12 19:56:39 -0700

    Adds include file path additions, to allow loading via new layout.

--- install/delegator.php
+++ install/delegator.php
@@ -1,2 +1,10 @@
 <?php
-include "chronicleMD.php";
+
+define('CHRONICLEMD_ROOT', realpath(dirname(__FILE__) . '/../'));
+define('BLOG_ROOT', realpath($_SERVER['DOCUMENT_ROOT']));
+
+set_include_path(get_include_path()
+	. PATH_SEPARATOR . CHRONICLEMD_ROOT
+	. PATH_SEPARATOR . BLOG_ROOT);
+
+include 'chronicleMD.php';
